2f7ab2d2cda305c1169b901e32c41eaf5447a6ab
hs242424 <79582976+hs242424@users.noreply.github.com>
2021-03-02 17:55:42 -0500

    Update main.py

--- main.py
+++ main.py
@@ -4,10 +4,16 @@ import random as r
 #alphabet for basic functions
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
 #final scramble code
-scrambledAlpabet =[]
-def scrambler():
-    helperAlphabet = alphabet
-    for x in alphabet:
-        currentVal = helperAlphabet[0]
-        print(currentVal)
-scrambler()
+def reset():
+    global scrambledAlpabet
+    scrambledAlpabet =[]
+reset()
+def scrambler(x):
+    firstHalfAlf = alphabet[0:13]
+    secondHalfAlf = alphabet[13:26]
+    print(firstHalfAlf)
+    print(secondHalfAlf)
+scrambler(12)
+'''
+using a place holder value that can be made using keypresses or gets exchanged with another place holder value you can replace characters in a string
+'''
