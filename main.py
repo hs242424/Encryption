84031de3230468d77395841f7beab9477c0d5c27
hs242424 <79582976+hs242424@users.noreply.github.com>
2021-03-02 13:34:10 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
+#required modulels
+import random as r
+
+#alphabet for basic functions
 alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+#final scramble code
 scrambledAlpabet =[]
+def scrambler():
+    helperAlphabet = alphabet
+    for x in alphabet:
+        currentVal = helperAlphabet[0]
+        print(currentVal)
+scrambler()
