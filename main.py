b8515ecb979025ac2c0141f3881366e384e9e392
hs242424 <79582976+hs242424@users.noreply.github.com>
2021-03-05 09:10:59 -0500

    Update main.py

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ def reset():
     global scrambledAlpabet
     scrambledAlpabet =[]
 reset()
+#Scrables first half and second half of the alphabet making a double slide cifer or sifer or scifer of whatever if is called, IDEs don't have autocorrect
 def scrambler():
     global scrambleVal
     scrambleVal = r.randrange(3,11)
@@ -21,7 +22,7 @@ def scrambler():
         firstHalfAlf.pop(0)
         firstHalfAlf.append(helpVal1)
         print(firstHalfAlf)
-        couterVal = 13
+        counterVal += 1
 scrambler()
 '''
 using a place holder value that can be made using keypresses or gets exchanged with another place holder value you can replace characters in a string
